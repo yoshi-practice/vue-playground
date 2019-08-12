78d6c7e8301fb7b8c57054998c0f3388b57e4878
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-13 01:26:18 +0900

    Update app-6

--- main.js
+++ main.js
@@ -35,3 +35,15 @@ var app4 = new Vue({
         ]
     }
 })
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'hello, vue.js'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
